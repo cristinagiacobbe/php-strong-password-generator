d4b539c8727097afc19555a2c5b7dfb11ba8d0ff
cristinagiacobbe <crigiak@gmail.com>
2024-03-07 23:03:31 +0100

    Add function.php (with logic)

---
+++ function.php
@@ -0,0 +1,31 @@
+<?php
+
+function generateRandPass($length)
+{
+    $possibileChar = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890|!£$%&/()=?^";
+    $password = "";
+
+    for ($i = 0; $i < $length; $i++) {
+        $password .= substr($possibileChar, rand(0, strlen($possibileChar) - 1), 1);
+    }
+    //header('Location: redirectPass.php');
+    return $password;
+};
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Function</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
