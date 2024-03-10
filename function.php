a9e6ef6e99346ed68f8f9ea691dd9e3afe776644
cristinagiacobbe <crigiak@gmail.com>
2024-03-10 12:01:29 +0100

    Add all conditions

--- function.php
+++ function.php
@@ -11,10 +11,90 @@ function generateRandPass($length, $repeat, $charL, $charN, $charS)
     $newpassword = "";
 
     while (strlen($password) < $length) {
+
+        //solo lettere
         if (isset($charL) && !isset($charN) && !isset($charS)) {
             $newpassword = substr(($possibileCharL), rand(0, strlen(($possibileCharL)) - 1), 1);
-            include __DIR__ . '/repeatFunction.php';
-            reapeatValues($repeat, $password, $newpassword);
+            //repeat?    
+            if ($repeat === "no") {
+                if (str_contains($password, $newpassword) === false) {
+                    $password .= $newpassword;
+                }
+            } else {
+                $password .= $newpassword;
+            }
+
+            //solo numeri
+        } else if (!isset($charL) && isset($charN) && !isset($charS)) {
+            $newpassword = substr(($possibileCharN), rand(0, strlen(($possibileCharN)) - 1), 1);
+            //repeat?    
+            if ($repeat === "no") {
+                if (str_contains($password, $newpassword) === false) {
+                    $password .= $newpassword;
+                }
+            } else {
+                $password .= $newpassword;
+            }
+
+            //solo simboli
+        } else if (!isset($charL) && !isset($charN) && isset($charS)) {
+            $newpassword = substr(($possibileCharS), rand(0, strlen(($possibileCharS)) - 1), 1);
+            //repeat?    
+            if ($repeat === "no") {
+                if (str_contains($password, $newpassword) === false) {
+                    $password .= $newpassword;
+                }
+            } else {
+                $password .= $newpassword;
+            }
+
+            //Lettere e numeri
+        } else if (isset($charL) && isset($charN) && !isset($charS)) {
+            $newpassword = substr(($possibileCharL . +$possibileCharN), rand(0, strlen(($possibileCharL . +$possibileCharN)) - 1), 1);
+            //repeat?    
+            if ($repeat === "no") {
+                if (str_contains($password, $newpassword) === false) {
+                    $password .= $newpassword;
+                }
+            } else {
+                $password .= $newpassword;
+            }
+
+            //Numeri e simboli
+        } else if (!isset($charL) && isset($charN) && isset($charS)) {
+            $newpassword = substr(($possibileCharN . +$possibileCharS), rand(0, strlen(($possibileCharN . +$possibileCharS)) - 1), 1);
+            //repeat?    
+            if ($repeat === "no") {
+                if (str_contains($password, $newpassword) === false) {
+                    $password .= $newpassword;
+                }
+            } else {
+                $password .= $newpassword;
+            }
+
+            // Lettere e simboli
+        } else if (isset($charL) && !isset($charN) && isset($charS)) {
+            $newpassword = substr(($possibileCharL . +$possibileCharS), rand(0, strlen(($possibileCharL . +$possibileCharS)) - 1), 1);
+            //repeat?    
+            if ($repeat === "no") {
+                if (str_contains($password, $newpassword) === false) {
+                    $password .= $newpassword;
+                }
+            } else {
+                $password .= $newpassword;
+            }
+
+            //Lettere, numeri e simboli
+        } else {
+            $newpassword = substr(($possibileCharL . +$possibileCharN . +$possibileCharS), rand(0, strlen(($possibileCharL . +$possibileCharN . +$possibileCharS)) - 1), 1);
+            //repeat?    
+            if ($repeat === "no") {
+                if (str_contains($password, $newpassword) === false) {
+                    $password .= $newpassword;
+                }
+            } else {
+                $password .= $newpassword;
+            }
         }
     }
     return $password;
